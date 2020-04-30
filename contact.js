99d306887ae6737278bd9ae0f919a8f5fdcc3a03
Rajath-55 <f20190122@goa.bits-pilani.ac.in>
2020-04-30 23:41:47 +0530

    changes

---
+++ contact.js
@@ -0,0 +1,9 @@
+function showemail() {
+    var email = document.getElementById("email");
+    email.innerHTML("1.rajath.v.05@gmail.com\n2.f20190122@goa.bits-pilani.ac.in")
+}
+
+function showcotact() {
+    var contact = document.getElementById("contact");
+    contact.innerText("Haha no")
+}
